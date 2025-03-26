2be1152bfe59e140d3f5f4bc928bd80713bbcf4d
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-03-26 20:06:22 +0530

    feat(UI): add launching screen layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.madassignment2"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.madassignment2"
