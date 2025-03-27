cf047756defe9e4f4f5d049885ce20d8c87aaf70
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-03-27 12:06:49 +0530

    feat (fix) : java class converted to kotlin

--- app/src/main/java/com/example/madassignment2/onBoardingScreen2.java
+++
@@ -1,6 +0,0 @@
-package com.example.madassignment2;
-
-import android.app.Activity;
-
-public class onBoardingScreen2 extends Activity {
-}
--- app/src/main/java/com/example/madassignment2/onBordingScreen3.java
+++
@@ -1,6 +0,0 @@
-package com.example.madassignment2;
-
-import android.app.Activity;
-
-public class onBordingScreen3 extends Activity {
-}
