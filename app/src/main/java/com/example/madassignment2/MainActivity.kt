7f66c5a38eaad8760b95ef1766bb19b47b29b52e
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-03-29 10:37:52 +0530

    feat(UI) : Onboarding Screens intent part

--- app/src/main/java/com/example/madassignment2/MainActivity.kt
+++ app/src/main/java/com/example/madassignment2/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.madassignment2
 
+import android.content.Intent
+import android.media.Image
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+
+        val launchImageView = findViewById<ImageView>(R.id.imageView5)
+
+        launchImageView.setOnClickListener {
+            // Start a new activity when the image is clicked
+            val intent = Intent(this, onBordingScreen1::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
--- app/src/main/java/com/example/madassignment2/onBoardingScreen2.kt
+++ app/src/main/java/com/example/madassignment2/onBoardingScreen2.kt
@@ -10,12 +10,6 @@ class onBoardingScreen2 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_on_boarding_screen2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.activity_on_boarding_screen2)  // Your new layout for the second screen
     }
 }
